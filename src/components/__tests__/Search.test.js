001db5096933c7dd9010ae091b845deca0406e5b
Mokarram Shahban <freedomshahban07@gmail.com>
2026-02-28 16:52:14 +0530

    Test Cases: for filter buttons

--- src/components/__tests__/Search.test.js
+++ src/components/__tests__/Search.test.js
@@ -1,9 +1,8 @@
-import { act, screen, render } from "@testing-library/react";
+import { act, screen, render, fireEvent } from "@testing-library/react";
 import MOCK_DATA from "../__mocks__/mockResListData.json";
 import Body from "../Body";
 import { BrowserRouter } from "react-router";
 import "@testing-library/jest-dom";
-
 
 global.fetch = jest.fn(() => {
   return Promise.resolve({
@@ -13,16 +12,43 @@ global.fetch = jest.fn(() => {
   });
 });
 
-it("Should render 20 cards", async () => {
+it("Should search restaurant list for burger text input", async () => {
   await act(async () =>
     render(
       <BrowserRouter>
         <Body />
-      </BrowserRouter>
-    )
+      </BrowserRouter>,
+    ),
   );
 
   const cardsButtonSearch = screen.getAllByTestId("resCard");
-  expect(cardsButtonSearch.length).toBe(20)
+  expect(cardsButtonSearch.length).toBe(20);
 
+  const searchBtn = screen.getByRole("button", { name: "Search" });
+  const searchInput = screen.getByTestId("searchInput");
+
+  fireEvent.change(searchInput, { target: { value: "burger" } });
+  fireEvent.click(searchBtn);
+
+  const cardsAfterSearch = screen.getAllByTestId("resCard");
+  expect(cardsAfterSearch.length).toBe(2);
+});
+
+it("Should filter top rated restaurants", async () => {
+  await act(async () =>
+    render(
+      <BrowserRouter>
+        <Body />
+      </BrowserRouter>,
+    ),
+  );
+
+  const cardsBeforeFilter = screen.getAllByTestId("resCard");
+  expect(cardsBeforeFilter.length).toBe(20);
+
+  const topRatedBtn = screen.getByRole("button", {name: "Top Rated Restaurants"})
+  fireEvent.click(topRatedBtn);
+
+  const cardsAfterFilter = screen.getAllByTestId("resCard");
+  expect(cardsAfterFilter.length).toBe(6);
 });
