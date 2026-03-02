e168556d4094473d2ab907816ef713ca705a523e
Mokarram Shahban <freedomshahban07@gmail.com>
2026-03-02 23:54:21 +0530

    Test Case: for cart (case-1)

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -11,6 +11,7 @@ const ItemList = ({ items }) => {
     <div>
       {items.map((item) => (
         <div
+        data-testid="foodItems"
           key={item?.card?.info?.id}
           className="p-2 m-2 border-b-2 border-gray-200 text-left text-lg py-2 flex"
         >
---
+++ src/components/__tests__/Cart.test.js
@@ -0,0 +1,32 @@
+import { render, screen, act, fireEvent } from "@testing-library/react"
+import { Provider } from "react-redux";
+import appStore from "../../utils/appStore";
+import RestaurantMenu from "../RestaurantMenu"
+import MOCK_DATA from "../__mocks__/mockResMenu.json"
+import "@testing-library/jest-dom"
+
+
+
+
+global.fetch = jest.fn( () => 
+Promise.resolve({
+    json: () => Promise.resolve(MOCK_DATA)
+}))
+
+it("Should load restaurant menu component", async () => {
+  await act(async () =>
+    render(
+      <Provider store={appStore}>
+        <RestaurantMenu />
+      </Provider>
+    )
+  );
+
+const accordionHeader = screen.getByText("Recommended (19)")
+fireEvent.click(accordionHeader);
+
+const categoryList = screen.getAllByTestId("foodItems")
+expect(categoryList.length).toBeInTheDocument(19)
+
+
+});
